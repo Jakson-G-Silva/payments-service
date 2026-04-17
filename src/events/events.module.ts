28721ddd54f6833a8cd8c30442f769831a3d8795
Jakson-G-Silva <jaksonguilherme2@gmail.com>
2026-04-17 19:16:22 -0300

    feat: add DLQ service for handling dead letter queue operations

--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -3,9 +3,15 @@ import { ConfigModule } from '@nestjs/config';
 import { RabbitmqService } from './rabbitmq/rabbitmq.service';
 import { PaymentQueueService } from './payment-queue/payment-queue.service';
 import { PaymentConsumerService } from './payment-consumer/payment-consumer.service';
+import { DlqService } from './dlq/dlq.service';
 
 @Module({
   imports: [ConfigModule],
-  providers: [RabbitmqService, PaymentQueueService, PaymentConsumerService],
+  providers: [
+    RabbitmqService,
+    PaymentQueueService,
+    PaymentConsumerService,
+    DlqService,
+  ],
 })
 export class EventsModule {}
